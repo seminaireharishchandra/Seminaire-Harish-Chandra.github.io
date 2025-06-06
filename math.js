0daf563543e7064f18189e52305a99e0dce3bfcf
seminaireharishchandra <106445751+seminaireharishchandra@users.noreply.github.com>
2025-06-06 17:40:52 +0545

    Update math.js

--- math.js
+++ math.js
@@ -1,10 +1,13 @@
-MathJax = {
-        tex: {
-            inlineMath: [
-                ['$', '$']
-            ],
-            displayMath: [
-                ['\\[', '\\]']
-            ],
-        }
-    };
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
